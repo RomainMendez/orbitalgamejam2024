c5649d89926af7d452ed0256672ee0e9f0d49bcb
Alban <fav.alban2@gmail.com>
2024-03-09 15:59:10 +0100

    charge ennemy WIP

--- projet_godot/enemy.gd
+++ projet_godot/enemy.gd
@@ -3,28 +3,75 @@ extends CharacterBody3D
 
 @onready var animated_sprite_3d = $AnimatedSprite3D
 
-@export var move_speed = 2.0
+@export var move_speed = 1.0
 @export var attack_range = 2.0
 
+enum State {
+	PREPARE_CHARGE,
+	CHARGE,
+	STUNNED,
+	PATROLLING,
+	DEAD
+}
+
 @onready var player : CharacterBody3D = get_tree().get_first_node_in_group("player")
-var dead = false
+@onready var current_state : State = State.PATROLLING
+
+
+
+var charge_range = 3.0
+var my_timer = 0.0
+var dir = Vector3(0.0,0.0,0.0)
+
 
 # damage per hit
 const damage: int = 1
 
 func _physics_process(delta):
-	if dead:
-		return
 	if player == null:
 		return
-	
-	var dir = player.global_position - global_position
-	dir.y = 0.0
-	dir = dir.normalized()
-	
-	velocity = dir * move_speed
-	move_and_slide()
-	attempt_to_kill_player()
+
+
+	match current_state:
+		State.DEAD:
+			return
+		State.PREPARE_CHARGE:
+			print("Preparing charge: timer: " + str(my_timer))
+			my_timer -=1 ;
+			if(my_timer == 0):
+				current_state=State.CHARGE;
+				animated_sprite_3d.play("dash");
+
+		State.CHARGE:
+			print("charging, dir: " + str(dir))
+			var collision = move_and_collide(dir * delta)
+			if (collision):
+				current_state=State.STUNNED;
+				animated_sprite_3d.play("death")
+				my_timer = 1000.0;
+
+		State.STUNNED:
+			print("stunned, timer: " + str(my_timer));
+			my_timer -=1;
+			if(my_timer == 0):
+				current_state=State.PATROLLING;
+
+		State.PATROLLING:
+			print("patrolling");
+			dir = player.global_position - global_position
+			dir.y = 0.0
+			dir = dir.normalized()
+			var dist_to_player = global_position.distance_to(player.global_position)
+
+			if dist_to_player < charge_range:
+				my_timer = 500.0
+				current_state=State.PREPARE_CHARGE
+				return
+			
+			velocity = dir * move_speed
+			move_and_slide()
+
+
 
 func attempt_to_kill_player():
 	var dist_to_player = global_position.distance_to(player.global_position)
@@ -38,7 +85,7 @@ func attempt_to_kill_player():
 		player.hurt(damage)
 
 func kill():
-	dead = true
+	current_state=State.DEAD
 	$DeathSound.play()
 	animated_sprite_3d.play("death")
 	$CollisionShape3D.disabled = true
