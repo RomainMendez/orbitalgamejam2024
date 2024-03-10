48e1ef372b189319871a53ad8da0e284b70f3aa2
Romai Mendez <romain.mendez@protonmail.com>
2024-03-10 11:30:33 +0100

    Added grenouille animations

--- projet_godot/shooting_enemy.gd
+++ projet_godot/shooting_enemy.gd
@@ -14,14 +14,25 @@ var until_next_shoot = DELAY_BETWEEN_SHOOT
 # At which range does the enemy fire
 @export var RANGE_TO_FIRE_FROM = 25.0;
 
+@onready var animations : AnimatedSprite3D = $AnimatedSprite3D
+
 var dead = false;
 
 @onready var player: CharacterBody3D = get_tree().get_first_node_in_group("player")
+
+func _ready():
+	animations.connect("animation_finished", Callable(self, "playNextAnim").bind(animations))
+
+func playNextAnim(animations_local: AnimatedSprite3D):
+	print("Caught you !")
+	if animations_local.get_animation() == "shooting":
+		animations_local.play("idle")
 
 func can_shoot_again():
 	return until_next_shoot < 0
 
 func shoot():
+	animations.play("shooting")
 	# Case if there's no player
 	if player == null:
 		return
@@ -62,5 +73,5 @@ func _physics_process(delta):
 
 func kill():
 	dead = true
-	$AnimatedSprite3D.play("death")
+	$AnimatedSprite3D.play("hurt")
 	player.score += 1
